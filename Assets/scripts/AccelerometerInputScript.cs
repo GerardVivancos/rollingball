b4646e19074b99b70f11ec9113bb90232f0401d7
Gerard Vivancos <gerard.vivancos@gmail.com>
2016-04-15 13:02:24 +0200

    Add proper accelerometer support and iOS profile

---
+++ Assets/scripts/AccelerometerInputScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AccelerometerInputScript : MonoBehaviour {
+
+	public float speed;
+	public GameObject textCanvas;
+
+	private Rigidbody rb;
+	private Text text;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		text = textCanvas.GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = (float) System.Math.Round(Input.acceleration.x, 1);
+		float moveVertical = (float) System.Math.Round(Input.acceleration.y, 1);
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		if (movement.ToString() != null) {
+			text.text = "(X: " + Input.acceleration.x + ", Y: " + Input.acceleration.y + ", Z: " + Input.acceleration.z + ")\n moveHorizontal: " + moveHorizontal + " moveVertical:" +moveVertical;
+		}
+
+		rb.AddForce (movement * speed);
+	}
+}
